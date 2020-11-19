fe2eb0e5e76e4a0b4898a5a4543dd6783ac31a38
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-11-20 00:39:01 +0300

    Table View Controller for Persons connected to datasource

--- Match Race Officer MVC/PersonTableViewController.swift
+++ Match Race Officer MVC/PersonTableViewController.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 class PersonTableViewController: UITableViewController {
-
+    
+    var persons: [Person] = [
+        Person(ifPersonId: "sweak1".uppercased(), familyName: "Kjellberg", givenName: "Anna", gender: .female),
+        Person(ifPersonId: "swemk".uppercased(), familyName: "Källström", givenName: "Malin", gender: .female),
+//        <Person FamilyName="Skarp" GivenName="Helena" IFPersonID="swehs2" NOC="SWE" PersonID="swehs2"/>
+//        <Person FamilyName="Tunnicliffe" GivenName="Anna" IFPersonID="usaat1" NOC="USA" PersonID="usaat1"/>
+//        <Person FamilyName="O'Bryan" GivenName="Molly" IFPersonID="usamo13" NOC="USA" PersonID="usamo13"/>
+//        <Person FamilyName="Capozzi" GivenName="Debbie" IFPersonID="usadc11" NOC="USA" PersonID="usadc11"/>
+//        <Person FamilyName="Wahlroos" GivenName="Chita" IFPersonID="fincw2" NOC="FIN" PersonID="fincw2"/>
+//        <Person FamilyName="Klemetz" GivenName="Maria" IFPersonID="finmk5" NOC="FIN" PersonID="finmk5"/>
+//        <Person FamilyName="Väresmaa" GivenName="Livia" IFPersonID="finlv1" NOC="FIN" PersonID="finlv1"/>
+//        <Person FamilyName="Leroy" GivenName="Claire" IFPersonID="fracl3" NOC="FRA" PersonID="fracl3"/>
+//        <Person FamilyName="Bertrand" GivenName="Elodie" IFPersonID="fraeb11" NOC="FRA" PersonID="fraeb11"/>
+//        <Person FamilyName="Riou" GivenName="Marie" IFPersonID="framr3" NOC="FRA" PersonID="framr3"/>
+        Person(ifPersonId: "ruses1".uppercased(), familyName: "Skudina", givenName: "Ekaterina", gender: .female),
+        Person(ifPersonId: "ruses5".uppercased(), familyName: "Syuzeva", givenName: "Elena", gender: .female)
+//        <Person FamilyName="Gaponovich" GivenName="Natalia" IFPersonID="rusng1" NOC="RUS" PersonID="rusng1"/>
+//        <Person FamilyName="Tulloch" GivenName="Genny" IFPersonID="usagt8" NOC="USA" PersonID="usagt8"/>
+    ]
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,24 +43,33 @@ class PersonTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        print(#line,#function,"Number of sections: \(1)")
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        print(#line,#function,"Section: \(section)")
+        if section == 0 {
+//            #if DEBUG
+            print(#line,#function,persons.count)
+//            #endif
+            return persons.count
+        }
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
 
+        let person = persons[indexPath.row]
+        cell.textLabel?.text = "\(person.familyName) \(person.givenName)"
+        cell.detailTextLabel?.text = "\(person.ifPersonId) \(person.gender)"
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
--- Match Race Officer MVC/ViewController.swift
+++ Match Race Officer MVC/ViewController.swift
@@ -8,26 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var persons: [Person] = [
-        Person(ifPersonId: "sweak1".uppercased(), familyName: "Kjellberg", givenName: "Anna", gender: .female),
-        Person(ifPersonId: "swemk".uppercased(), familyName: "Källström", givenName: "Malin", gender: .female),
-//        <Person FamilyName="Skarp" GivenName="Helena" IFPersonID="swehs2" NOC="SWE" PersonID="swehs2"/>
-//        <Person FamilyName="Tunnicliffe" GivenName="Anna" IFPersonID="usaat1" NOC="USA" PersonID="usaat1"/>
-//        <Person FamilyName="O'Bryan" GivenName="Molly" IFPersonID="usamo13" NOC="USA" PersonID="usamo13"/>
-//        <Person FamilyName="Capozzi" GivenName="Debbie" IFPersonID="usadc11" NOC="USA" PersonID="usadc11"/>
-//        <Person FamilyName="Wahlroos" GivenName="Chita" IFPersonID="fincw2" NOC="FIN" PersonID="fincw2"/>
-//        <Person FamilyName="Klemetz" GivenName="Maria" IFPersonID="finmk5" NOC="FIN" PersonID="finmk5"/>
-//        <Person FamilyName="Väresmaa" GivenName="Livia" IFPersonID="finlv1" NOC="FIN" PersonID="finlv1"/>
-//        <Person FamilyName="Leroy" GivenName="Claire" IFPersonID="fracl3" NOC="FRA" PersonID="fracl3"/>
-//        <Person FamilyName="Bertrand" GivenName="Elodie" IFPersonID="fraeb11" NOC="FRA" PersonID="fraeb11"/>
-//        <Person FamilyName="Riou" GivenName="Marie" IFPersonID="framr3" NOC="FRA" PersonID="framr3"/>
-        Person(ifPersonId: "ruses1".uppercased(), familyName: "Skudina", givenName: "Ekaterina", gender: .female),
-        Person(ifPersonId: "ruses5".uppercased(), familyName: "Syuzeva", givenName: "Elena", gender: .female)
-//        <Person FamilyName="Gaponovich" GivenName="Natalia" IFPersonID="rusng1" NOC="RUS" PersonID="rusng1"/>
-//        <Person FamilyName="Tulloch" GivenName="Genny" IFPersonID="usagt8" NOC="USA" PersonID="usagt8"/>
-    ]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
