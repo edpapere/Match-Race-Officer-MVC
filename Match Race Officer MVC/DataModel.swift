c4181bab9569cb85327f7e8544c004734ab57d4a
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-11-22 23:45:19 +0300

    Added view for EventStage aka Round-Robin

--- Match Race Officer MVC/DataModel.swift
+++ Match Race Officer MVC/DataModel.swift
@@ -44,7 +44,56 @@ struct Team {
 }
 
 struct Match {
-    var matchId: MatchID
-    var starboardTeam: TeamID
-    var portTeam: TeamID
+    var portSideTeam: TeamID
+    var portSideBoat: BoatID
+    var stbdSideTeam: TeamID
+    var stbdSideBoat: BoatID
 }
+
+struct Flight {
+    var matches: [Match]
+}
+
+struct EventStage {
+    var flights: [Flight]
+}
+
+let RoundRobin6S6B = EventStage(
+    flights: [
+        Flight(
+            matches: [
+                Match(portSideTeam: 1, portSideBoat: 1, stbdSideTeam: 2, stbdSideBoat: 2),
+                Match(portSideTeam: 3, portSideBoat: 3, stbdSideTeam: 4, stbdSideBoat: 4),
+                Match(portSideTeam: 5, portSideBoat: 5, stbdSideTeam: 6, stbdSideBoat: 6)
+            ]
+        ),
+        Flight(
+            matches: [
+                Match(portSideTeam: 1, portSideBoat: 1, stbdSideTeam: 4, stbdSideBoat: 4),
+                Match(portSideTeam: 6, portSideBoat: 6, stbdSideTeam: 2, stbdSideBoat: 2),
+                Match(portSideTeam: 5, portSideBoat: 5, stbdSideTeam: 3, stbdSideBoat: 3)
+            ]
+        ),
+        Flight(
+            matches: [
+                Match(portSideTeam: 6, portSideBoat: 6, stbdSideTeam: 1, stbdSideBoat: 1),
+                Match(portSideTeam: 4, portSideBoat: 4, stbdSideTeam: 5, stbdSideBoat: 5),
+                Match(portSideTeam: 3, portSideBoat: 3, stbdSideTeam: 2, stbdSideBoat: 2)
+            ]
+        ),
+        Flight(
+            matches: [
+                Match(portSideTeam: 5, portSideBoat: 5, stbdSideTeam: 1, stbdSideBoat: 1),
+                Match(portSideTeam: 6, portSideBoat: 6, stbdSideTeam: 3, stbdSideBoat: 3),
+                Match(portSideTeam: 2, portSideBoat: 2, stbdSideTeam: 4, stbdSideBoat: 4)
+            ]
+        ),
+        Flight(
+            matches: [
+                Match(portSideTeam: 1, portSideBoat: 1, stbdSideTeam: 3, stbdSideBoat: 3),
+                Match(portSideTeam: 4, portSideBoat: 6, stbdSideTeam: 6, stbdSideBoat: 4),
+                Match(portSideTeam: 2, portSideBoat: 2, stbdSideTeam: 5, stbdSideBoat: 5)
+            ]
+        )
+    ]
+)
---
+++ Match Race Officer MVC/EventStageTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  EventStageTableViewController.swift
+//  Match Race Officer MVC
+//
+//  Created by Andrey Pereslavtsev on 22.11.2020.
+//
+
+import UIKit
+
+class EventStageTableViewController: UITableViewController {
+
+    var match = Match(portSideTeam: 1, portSideBoat: 1, stbdSideTeam: 2, stbdSideBoat: 2)
+    
+    var eventStage = RoundRobin6S6B
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return eventStage.flights.count
+        // return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return eventStage.flights[section].matches.count
+        //return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let match = eventStage.flights[indexPath.section].matches[indexPath.row]
+        
+        var portWillChange = false
+        var stbdWillChange = false
+        
+        if indexPath.section < eventStage.flights.count-1 {
+            for next in eventStage.flights[indexPath.section+1].matches {
+                portWillChange =
+                    portWillChange ||
+                    ( match.portSideBoat == next.portSideBoat && match.portSideTeam != next.portSideTeam ) ||
+                    ( match.portSideBoat == next.stbdSideBoat && match.portSideTeam != next.stbdSideTeam )
+                stbdWillChange =
+                    stbdWillChange ||
+                    ( match.stbdSideBoat == next.portSideBoat && match.stbdSideTeam != next.portSideTeam ) ||
+                    ( match.stbdSideBoat == next.stbdSideBoat && match.stbdSideTeam != next.stbdSideTeam )
+            }
+        }
+        
+        cell.textLabel?.text = "B\(match.portSideBoat):Skip\(match.portSideTeam):\(portWillChange)"
+        cell.detailTextLabel?.text = "B\(match.stbdSideBoat):Skip\(match.stbdSideTeam):\(stbdWillChange)"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Flight \(section+1)"
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
