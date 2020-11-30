9e6d1eba5d80ab72718116480dc9aeb3f9ceffb0
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-12-01 00:39:37 +0300

    Removed some deprecated code from Headed Table View Controller

--- Match Race Officer MVC/HeadedTableViewController.swift
+++ Match Race Officer MVC/HeadedTableViewController.swift
@@ -128,36 +128,8 @@ class HeadedTableViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! FlightTableViewCell
 
-        // Configure the cell...
-        let match = array.flights[indexPath.section].matches[indexPath.row]
-        
-        
-        var portJustChanged = false
-        var stbdJustChanged = false
-        
-        if indexPath.section > 0 {
-            for previous in array.flights[indexPath.section-1].matches {
-                portJustChanged =
-                    portJustChanged ||
-                    ( match.portSideBoat == previous.portSideBoat && match.portSideTeam != previous.portSideTeam ) ||
-                    ( match.portSideBoat == previous.stbdSideBoat && match.portSideTeam != previous.stbdSideTeam )
-                stbdJustChanged =
-                    stbdJustChanged ||
-                    ( match.stbdSideBoat == previous.portSideBoat && match.stbdSideTeam != previous.portSideTeam ) ||
-                    ( match.stbdSideBoat == previous.stbdSideBoat && match.stbdSideTeam != previous.stbdSideTeam )
-            }
-        }
-        
-        // cell.update(with: match, as: indexPath.row, markPort: portJustChanged, markStbd: stbdJustChanged)
-
         cell.update(with: array, indexPath: indexPath) //, markBoatChanges: true
-
-        
-        //textFieldCell.layer.borderColor = UIColor.orange.cgColor
-        //cell.layer.borderColor = UIColor.orange.cgColor
-        //cell.layer.borderWidth = 2
-        
-      
+              
         return cell
     }
     
